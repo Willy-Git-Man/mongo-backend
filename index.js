9fb58aec8001ecbfb088be82d2edc79a2f296563
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-05-16 17:36:39 -0400

    connected to mongo db working

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express'; //non named export not in obj
+import mongoose from 'mongoose';
+import { MONGO_URI } from './config/keys.js';
 import { questions } from './routes/questions.js'; //named exports is insdie obj
 
 const app = express();
 //goes into express node module and grabs from index.js which here is just '/lib/express'
 app.use(express.json())
+
+mongoose
+.connect(MONGO_URI, {useNewUrlParser:true, useUnifiedTopology: true})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err))
 
 // app.get('/', (req, res) => {
 //   res.send('hello world test')
